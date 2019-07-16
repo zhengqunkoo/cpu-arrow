a263d1b36649e1f14a33a87243739b268bb2359b
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-07-16 12:01:58 +0800

    Move BoolAlgebra to its own file

--- src/CpuArrow/Logic.hs
+++ src/CpuArrow/Logic.hs
@@ -3,37 +3,7 @@ module CpuArrow.Logic where
 import Control.Arrow
 import Control.Monad
 import CpuArrow.Base
-
-class BoolAlgebra a where
-    top :: a
-    bottom :: a
-
-    bAnd :: a -> a -> a
-    bOr :: a -> a -> a
-    bNot :: a -> a
-    bNand :: a -> a -> a
-    bNand x y = bNot (bAnd x y)
-    bXor :: a -> a -> a
-    bXor x y = bOr (bAnd x (bNot y)) (bAnd (bNot x) y)
-
-    dAnd :: Bool -> a -> a
-    dAnd b x = if b then x else bottom
-    dOr :: Bool -> a -> a
-    dOr b x = if b then top else x
-
-instance BoolAlgebra Bool where
-    top = True
-    bottom = False
-    bAnd = (&&)
-    bOr = (||)
-    bNot = not
-
-instance BoolAlgebra a => BoolAlgebra [a] where
-    top = top:top
-    bottom = bottom:bottom
-    bAnd = liftM2 bAnd
-    bOr = liftM2 bOr
-    bNot = liftM bNot
+import CpuArrow.BoolAlgebra
 
 aAnd :: BoolAlgebra a => Circuit (a, a) a
 aAnd = arr $ uncurry bAnd
