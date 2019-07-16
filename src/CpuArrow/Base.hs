33ef58461a2c93c248eaaa6e948c2619a56a7d12
zhengqunkoo <root@zhengqunkoo.com>
2019-07-16 09:54:14 +0800

    Define runnable datatype as arrow instance

---
+++ src/CpuArrow/Base.hs
@@ -0,0 +1,26 @@
+module CpuArrow.Base where
+
+import Control.Arrow
+import Control.Category
+
+newtype Circuit b c =
+  Circuit
+    { unCircuit :: b -> c
+    }
+
+instance Category Circuit where
+  id = Circuit Prelude.id
+  (.) (Circuit f) (Circuit g) = Circuit $ (Prelude..) f g
+
+instance Arrow Circuit where
+  arr f = Circuit f
+  first (Circuit f) =
+    Circuit $
+    (\(b, d) ->
+       let c = f b
+        in (c, d))
+
+runCircuit :: Circuit b c -> b -> c
+runCircuit cir b = f b
+  where
+    f = unCircuit cir
