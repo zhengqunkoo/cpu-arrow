f6a48612b9b4415af454c00c8a490a494d0c3bae
zhengqunkoo <root@zhengqunkoo.com>
2019-07-16 22:41:14 +0800

    Operate Circuit over infinite sequences
    Revert test specification: yet to prove equality on sequences

--- src/CpuArrow/Base.hs
+++ src/CpuArrow/Base.hs
@@ -3,9 +3,25 @@ module CpuArrow.Base where
 import Control.Arrow
 import Control.Category
 
+data Seq a =
+  Cons a (Seq a)
+
+instance Functor Seq where
+  fmap f (Cons a as) = Cons (f a) $ fmap f as
+
+class Zippable f where
+  zipTo :: (f a, f b) -> f (a, b)
+  zipFrom :: f (a, b) -> (f a, f b)
+
+instance Zippable Seq where
+  zipTo (Cons a as, Cons b bs) = Cons (a, b) $ zipTo (as, bs)
+  zipFrom (Cons (a, b) ts) = (Cons a as, Cons b bs)
+    where
+      (as, bs) = zipFrom ts
+
 newtype Circuit b c =
   Circuit
-    { unCircuit :: b -> c
+    { unCircuit :: Seq b -> Seq c
     }
 
 instance Category Circuit where
@@ -13,15 +29,19 @@ instance Category Circuit where
   (.) (Circuit f) (Circuit g) = Circuit $ (Prelude..) f g
 
 instance Arrow Circuit where
-  arr f = Circuit f
-  first (Circuit f) =
-    Circuit $
-    (\(b, d) ->
-       let c = f b
-        in (c, d))
+  arr f = Circuit $ fmap f
+  first (Circuit f) = Circuit $ zipTo `dot` (\(b, d) -> (f b, d)) `dot` zipFrom
+    where
+      dot = (Control.Category..)
 
-runCircuit :: Circuit b c -> b -> c
-runCircuit cir b = f b
+on :: Seq Bool
+on = Cons True on
+
+off :: Seq Bool
+off = Cons False off
+
+runCircuit :: Circuit b c -> Seq b -> Seq c
+runCircuit cir bs = f bs
   where
     f = unCircuit cir
 
