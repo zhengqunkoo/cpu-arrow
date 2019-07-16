122861fce25f730c02bbcf81f8cbb1e2d895d4d8
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-07-16 11:37:20 +0800

    Generalise logic gates to work on any boolean algebra

--- src/CpuArrow/Logic.hs
+++ src/CpuArrow/Logic.hs
@@ -1,19 +1,51 @@
 module CpuArrow.Logic where
 
 import Control.Arrow
+import Control.Monad
 import CpuArrow.Base
 
-aAnd :: Circuit (Bool, Bool) Bool
-aAnd = arr $ uncurry (&&)
+class BoolAlgebra a where
+    top :: a
+    bottom :: a
 
-aOr :: Circuit (Bool, Bool) Bool
-aOr = arr $ uncurry (||)
+    bAnd :: a -> a -> a
+    bOr :: a -> a -> a
+    bNot :: a -> a
+    bNand :: a -> a -> a
+    bNand x y = bNot (bAnd x y)
+    bXor :: a -> a -> a
+    bXor x y = bOr (bAnd x (bNot y)) (bAnd (bNot x) y)
 
-aNot :: Circuit Bool Bool
-aNot = arr not
+    dAnd :: Bool -> a -> a
+    dAnd b x = if b then x else bottom
+    dOr :: Bool -> a -> a
+    dOr b x = if b then top else x
 
-aNand :: Circuit (Bool, Bool) Bool
-aNand = aNot *** aNot >>> aAnd
+instance BoolAlgebra Bool where
+    top = True
+    bottom = False
+    bAnd = (&&)
+    bOr = (||)
+    bNot = not
 
-aXor :: Circuit (Bool, Bool) Bool
+instance BoolAlgebra a => BoolAlgebra [a] where
+    top = top:top
+    bottom = bottom:bottom
+    bAnd = liftM2 bAnd
+    bOr = liftM2 bOr
+    bNot = liftM bNot
+
+aAnd :: BoolAlgebra a => Circuit (a, a) a
+aAnd = arr $ uncurry bAnd
+
+aOr :: BoolAlgebra a => Circuit (a, a) a
+aOr = arr $ uncurry bOr
+
+aNot :: BoolAlgebra a => Circuit a a
+aNot = arr bNot
+
+aNand :: BoolAlgebra a => Circuit (a, a) a
+aNand = aAnd >>> aNot
+
+aXor :: BoolAlgebra a => Circuit (a, a) a
 aXor = aNand &&& aOr >>> aAnd
