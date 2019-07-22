4105a84012596ad125aa18c82b8b01b5554a5126
zhengqunkoo <root@zhengqunkoo.com>
2019-07-23 00:03:57 +0800

    Import exposed modules, undefine main

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import CpuArrow.Advanced.Alu
+import CpuArrow.Advanced.Demux
+import CpuArrow.Advanced.FullAdder
+import CpuArrow.Advanced.HalfAdder
+import CpuArrow.Advanced.Mux
+import CpuArrow.Base
+import CpuArrow.BoolAlgebra
+import CpuArrow.Cpu
+import CpuArrow.Decoder
+import CpuArrow.Logic
+import CpuArrow.Memory.Counter
+import CpuArrow.Memory.Ram
+import CpuArrow.Memory.Register
