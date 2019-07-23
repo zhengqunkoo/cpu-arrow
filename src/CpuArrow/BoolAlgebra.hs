858b37a9f9785d9ab79b8bd4d3f8bba1c7fd9537
zhengqunkoo <root@zhengqunkoo.com>
2019-07-23 09:36:27 +0800

    Fix indentation

--- src/CpuArrow/BoolAlgebra.hs
+++ src/CpuArrow/BoolAlgebra.hs
@@ -3,33 +3,36 @@ module CpuArrow.BoolAlgebra where
 import Control.Monad
 
 class BoolAlgebra a where
-    top :: a
-    bottom :: a
-
-    bAnd :: a -> a -> a
-    bOr :: a -> a -> a
-    bNot :: a -> a
-    bNand :: a -> a -> a
-    bNand x y = bNot (bAnd x y)
-    bXor :: a -> a -> a
-    bXor x y = bOr (bAnd x (bNot y)) (bAnd (bNot x) y)
-
-    dAnd :: Bool -> a -> a
-    dAnd b x = if b then x else bottom
-    dOr :: Bool -> a -> a
-    dOr b x = if b then top else x
+  top :: a
+  bottom :: a
+  bAnd :: a -> a -> a
+  bOr :: a -> a -> a
+  bNot :: a -> a
+  bNand :: a -> a -> a
+  bNand x y = bNot (bAnd x y)
+  bXor :: a -> a -> a
+  bXor x y = bOr (bAnd x (bNot y)) (bAnd (bNot x) y)
+  dAnd :: Bool -> a -> a
+  dAnd b x =
+    if b
+      then x
+      else bottom
+  dOr :: Bool -> a -> a
+  dOr b x =
+    if b
+      then top
+      else x
 
 instance BoolAlgebra Bool where
-    top = True
-    bottom = False
-    bAnd = (&&)
-    bOr = (||)
-    bNot = not
+  top = True
+  bottom = False
+  bAnd = (&&)
+  bOr = (||)
+  bNot = not
 
 instance BoolAlgebra a => BoolAlgebra [a] where
-    top = top:top
-    bottom = bottom:bottom
-    bAnd = liftM2 bAnd
-    bOr = liftM2 bOr
-    bNot = liftM bNot
-
+  top = top : top
+  bottom = bottom : bottom
+  bAnd = liftM2 bAnd
+  bOr = liftM2 bOr
+  bNot = liftM bNot
