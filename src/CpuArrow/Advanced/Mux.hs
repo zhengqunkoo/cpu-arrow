5c1610673a4d3ebf8a938518da9cb380d040a088
zhengqunkoo <root@zhengqunkoo.com>
2019-07-25 07:25:11 +0800

    Define mux

--- src/CpuArrow/Advanced/Mux.hs
+++ src/CpuArrow/Advanced/Mux.hs
@@ -1,1 +1,25 @@
 module CpuArrow.Advanced.Mux where
+
+import Control.Arrow
+import CpuArrow.Base
+import CpuArrow.BoolAlgebra
+import CpuArrow.Logic
+
+aMux :: BoolAlgebra a => Circuit (T2 a, a) a
+aMux =
+  (first (arr snd) >>> aAnd) &&& (first (arr fst) >>> second aNot >>> aAnd) >>>
+  aOr
+
+aMux2 :: BoolAlgebra a => Circuit (T4 a, (a, a)) a
+aMux2 =
+  ((second (arr snd) >>>
+    ((first (arr halfFst) >>> aMux) &&& (first (arr halfSnd) >>> aMux))) &&&
+   second (arr fst)) >>>
+  (second (arr snd) >>> aMux)
+
+aMux3 :: BoolAlgebra a => Circuit (T8 a, (a, a, a)) a
+aMux3 =
+  ((second (arr (\(_, a, b) -> (a, b))) >>>
+    ((first (arr halfFst) >>> aMux2) &&& (first (arr halfSnd) >>> aMux2))) &&&
+   second (arr (\(a, _, _) -> a))) >>>
+  (second (arr snd) >>> aMux)
