61d3dd4af7576803f9b418039281dc7b96a6744b
zhengqunkoo <root@zhengqunkoo.com>
2019-07-25 07:23:36 +0800

    Define generic tuple views

--- src/CpuArrow/Base.hs
+++ src/CpuArrow/Base.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+
 module CpuArrow.Base where
 
 import Control.Arrow
@@ -18,6 +20,36 @@ instance Arrow Circuit where
     Circuit $ uncurry zip `dot` (\(b, d) -> (f b, d)) `dot` unzip
     where
       dot = (Control.Category..)
+
+type T2 a = (a, a)
+
+type T4 a = (a, a, a, a)
+
+type T8 a = (a, a, a, a, a, a, a, a)
+
+type T16 a = (a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a)
+
+class Half a b where
+  halfFst :: a -> b
+  halfSnd :: a -> b
+
+instance Half (T2 a) a where
+  halfFst = fst
+  halfSnd = snd
+
+instance Half (T4 a) (T2 a) where
+  halfFst (a1, a2, a3, a4) = (a1, a2)
+  halfSnd (a1, a2, a3, a4) = (a3, a4)
+
+instance Half (T8 a) (T4 a) where
+  halfFst (a1, a2, a3, a4, a5, a6, a7, a8) = (a1, a2, a3, a4)
+  halfSnd (a1, a2, a3, a4, a5, a6, a7, a8) = (a5, a6, a7, a8)
+
+instance Half (T16 a) (T8 a) where
+  halfFst (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16) =
+    (a1, a2, a3, a4, a5, a6, a7, a8)
+  halfSnd (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16) =
+    (a9, a10, a11, a12, a13, a14, a15, a16)
 
 on :: [Bool]
 on = True : on
