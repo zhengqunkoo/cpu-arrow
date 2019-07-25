8101b8674591099824f5e67ecc2d3e87d80e84e0
zhengqunkoo <root@zhengqunkoo.com>
2019-07-26 02:16:26 +0800

    Define fulladder

--- src/CpuArrow/Advanced/FullAdder.hs
+++ src/CpuArrow/Advanced/FullAdder.hs
@@ -1,1 +1,13 @@
 module CpuArrow.Advanced.FullAdder where
+
+import Control.Arrow
+import CpuArrow.Advanced.HalfAdder
+import CpuArrow.Base
+import CpuArrow.BoolAlgebra
+import CpuArrow.Logic
+
+aFullAdder :: BoolAlgebra a => Circuit ((a, a), a) (a, a)
+aFullAdder =
+  (arr fst >>> aHalfAdder) &&& arr snd >>>
+  ((arr (\((_, s'), c) -> (s', c)) >>> aHalfAdder) &&& arr (\((c', _), _) -> c')) >>>
+  ((arr (\((c'', _), c') -> (c'', c')) >>> aOr) &&& arr (\((_, s''), _) -> s''))
