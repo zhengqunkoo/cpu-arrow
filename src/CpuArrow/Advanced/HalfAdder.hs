123f994038ced9727374b2b145b53eb11ec947e9
zhengqunkoo <root@zhengqunkoo.com>
2019-07-25 10:32:46 +0800

    Define halfadder

--- src/CpuArrow/Advanced/HalfAdder.hs
+++ src/CpuArrow/Advanced/HalfAdder.hs
@@ -1,1 +1,9 @@
 module CpuArrow.Advanced.HalfAdder where
+
+import Control.Arrow
+import CpuArrow.Base
+import CpuArrow.BoolAlgebra
+import CpuArrow.Logic
+
+aHalfAdder :: BoolAlgebra a => Circuit (a, a) (a, a)
+aHalfAdder = aAnd &&& aXor
